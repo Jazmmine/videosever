dc44cd77a2a6324bbee9ff617ad0abd1c3651c0c
Jazmmine <jazmine.angoma@gmail.com>
2016-10-11 15:32:12 -0500

    subiendo con jquery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+/*window.addEventListener("load", function(){
 		var linksArray = document.querySelectorAll("a");
 		for (var i = 0; i < linksArray.length; i++) {
 			var linksArrayElemento = linksArray[i];
@@ -9,4 +9,29 @@ window.addEventListener("load", function(){
 				linksArrayElemento.appendChild(imgElemento);
 		}
 
-});
+});*/
+
+var cargar = function() {
+	var $enlaces = $("a");
+	$enlaces.each(iterar);
+};
+
+var iterar = function(index, element) {
+	var $enlace = $(element);
+	var linkVideo = $enlace.attr("href");
+	var linkImagen = youtube.generateThumbnailUrl(linkVideo);
+	var $img = $("<img>");
+	$img.attr("src", linkImagen);
+	$enlace.append($img);
+};
+
+/*
+$(document).ready(cargar);
+
+$(document).ready(function(){
+	var $linksArray = $("a");
+	for (var i = 0; i < linksArray.length; i++) {
+		var $linksArrayElemento = $(linksArray[i]);
+		var videoUrl = $linksArrayElemento.attr("href");
+	}
+});*/
